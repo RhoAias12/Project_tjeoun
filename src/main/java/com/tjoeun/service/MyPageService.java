e7d68b8ed3ec11f2ee06ee4fa7b0cd2ea402f3b0
tjoeunJava <dud-0823@naver.com>
2025-07-07 13:07:58 +0900

    지원현황 수정중

--- src/main/java/com/tjoeun/service/MyPageService.java
+++ src/main/java/com/tjoeun/service/MyPageService.java
@@ -43,6 +43,7 @@ public class MyPageService {
                     .statusDisplay(history.getStatus().getDisplay())
                     .recruitmentTitle(history.getRecruitment().getTitle())
                     .recruitmentCompany(history.getRecruitment().getCompany())
+                    .recruitmentId(history.getRecruitment().getRecruitmentIdx())
                     .resumeId(history.getOptionalIdx())
                     .build())
             .toList();
