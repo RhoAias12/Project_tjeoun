9eafec97ce4545fd1196c8b945c9c876b355f05d
tjoeunJava <dud-0823@naver.com>
2025-07-07 14:20:55 +0900

    지원현황 아직 수정중
    entity 수정함

--- src/main/java/com/tjoeun/entity/Users.java
+++ src/main/java/com/tjoeun/entity/Users.java
@@ -8,6 +8,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.sql.Timestamp;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -55,4 +57,10 @@ public class Users {
       .userRole(UserRole.USER)
       .build();
   }
+
+  @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
+  private List<Favorite> favorites = new ArrayList<>();
+
+  @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
+  private List<Resume> resumes = new ArrayList<>();
 }
